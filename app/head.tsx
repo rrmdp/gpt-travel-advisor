d98c292ea8baeedcc97dc7797a6a87ff15c167f0
Rodrigo Rocco <rocco.rodrigo@hotmail.com>
2023-03-01 01:00:29 +0000

    .

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,8 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Build your custom Mallorca itenerary powered by AI" />
       <link rel="icon" href="/favicon.ico" /> 
-      <AnalyticsWrapper />         
+      <AnalyticsWrapper /> 
+      <meta name="google-site-verification" content="BKOrUF74-QvCNriJNcT5bsRdJU_zMwrsZKTz97QSQaU" />        
     </>
   )
 }
