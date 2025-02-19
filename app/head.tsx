b07703d37dbd7999e15c5e83601fc29372b32422
Rodrigo Rocco <rocco.rodrigo@hotmail.com>
2025-02-19 20:55:18 +0000

    .

--- app/head.tsx
+++ app/head.tsx
@@ -5,11 +5,12 @@ export default function Head() {
     <>
       <title>What to do in Mallorca? Tourist itineraries by VillasMediterranean.com</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="What to do in Majorca? Build your custom touorist itenerary in Mallorca powered by AI" />
+      <meta name="description" content="What to do in Majorca? Build your custom tourist itinerary in Mallorca powered by AI" />
       <link rel="icon" href="/favicon.ico" /> 
       <AnalyticsWrapper /> 
       <meta name="google-site-verification" content="BKOrUF74-QvCNriJNcT5bsRdJU_zMwrsZKTz97QSQaU" />        
       <meta name="author" content="Rodrigo Rocco @rrmdp on Twitter"></meta>
+      <link rel="canonical" href="https://www.whattodoinmallorca.com" />
     </>
   )
 }
